a829ba4b43f59be0475f38046c406b87a1f65b92
José Manuel Corona Villán <ChemaCorona>
2019-01-17 09:17:54 +0100

    Ejercicios tema 1 - Laravel

---
+++ resources/views/catalog/edit.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Videoclub</title>
+	</head>
+	<body>
+		@extends('layouts.master')
+
+		@section('content')
+			Modificar película
+		@stop
+	</body>
+</html>
